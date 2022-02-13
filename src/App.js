feeeb17b63b2cd9976a6758bc3b3a2f919d9f569
Prateek Kumar <prateekx.kumar@intel.com>
2022-02-13 18:47:30 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -25,41 +25,13 @@ function App() {
   };
   useEffect(() => {
     getAllPokemons();
-  }, []);
-
-  return (
-    <div className="app-container">
-      <h1>Pokemon Kingdom .</h1>
-
-      <div className="pokemon-container">
-        <div className="all-container">
+      <div>
+        <div>
           {allPokemons.map((pokemon, index) => (
-            <PokemonThumbnail
-              id={pokemon.id}
-              name={pokemon.name}
-              image={pokemon.sprites.other.dream_world.front_default}
-              type={pokemon.types[0].type.name}
-              key={index}
-              height={pokemon.height}
-              weight={pokemon.weight}
-              stat1={pokemon.stats[0].stat.name}
-              stat2={pokemon.stats[1].stat.name}
-              stat3={pokemon.stats[2].stat.name}
-              stat4={pokemon.stats[3].stat.name}
-              stat5={pokemon.stats[4].stat.name}
-              stat6={pokemon.stats[5].stat.name}
-              bs1={pokemon.stats[0].base_stat}
-              bs2={pokemon.stats[1].base_stat}
-              bs3={pokemon.stats[2].base_stat}
-              bs4={pokemon.stats[3].base_stat}
-              bs5={pokemon.stats[4].base_stat}
-              bs6={pokemon.stats[5].base_stat}
-            />
+            <PokemonThumbnail />
           ))}
         </div>
-        <button className="load-more" onClick={() => getAllPokemons()}>
-          More Pokemons
-        </button>
+        <button onClick={() => getAllPokemons()}>More Pokemons</button>
       </div>
     </div>
   );
